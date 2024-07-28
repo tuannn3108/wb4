2d7c01e4660d3ed308e15f10422e8e94189aac8b
tuannn3108 <anhtuan3182002222@gmail.com>
2024-07-28 12:34:00 +0700

    ha

---
+++ bt4buoi4/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bt4buoi4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listView1.SelectedIndices.Count > 0)
+            {
+                ListViewItem listViewItem = listView1.SelectedItems[0];
+                string MSNV = listViewItem.SubItems[0].Text;
+                string TenSP=
+                listViewItem.SubItems[1].Text;
+                string LuongCB =
+               listViewItem.SubItems[2].Text;
+
+
+            }
+
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+
+            Form2 form2 = new Form2();
+            form2.DataSent += Form2_DataSent;
+            form2.Show(); // Hoặc form2.ShowDialog() nếu bạn muốn Form2 là form modal
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.DataSent += Form2_DataSent;
+            form2.Show(); // Hoặc form2.ShowDialog() nếu bạn muốn Form2 là form modal
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+
+            this.Close();
+
+
+        }
+        private void Form2_DataSent(string data1, string data2, string data3)
+        {
+            // Thêm dữ liệu vào ListView
+            ListViewItem item = new ListViewItem(data1);
+            item.SubItems.Add(data2);
+            item.SubItems.Add(data3);
+            listView1.Items.Add(item);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ bt4buoi4/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bt4buoi4
+{
+    public partial class Form2 : Form
+    {
+        // Khai báo một delegate
+        public delegate void DataSentEventHandler(string data1, string data2, string data3);
+        // Khai báo một sự kiện dựa trên delegate đó
+        public event DataSentEventHandler DataSent;
+        public Form2()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(button1_Click);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string MSNV = this.textBox1.Text.Trim();
+            string TenNV = this.textBox2.Text.Trim();
+            string Luongcb = this.textBox3.Text.Trim(); // Lưu ý: Chuyển sang kiểu string cho nhất quán
+
+            // Gửi dữ liệu thông qua sự kiện trước khi đóng form
+            DataSent?.Invoke(MSNV, TenNV, Luongcb);
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+
+}
